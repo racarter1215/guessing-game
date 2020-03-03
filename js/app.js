bd8adbe4c88aad87e562d5603963fc1a3eae558d
racarter1215 <racarter1215@gmail.com>
2020-03-03 15:26:02 -0800

    added nav bar to canvas and github profile pages, changed console logs for clarity

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 'use strict';
 
 var answer = prompt("What is your first name?");
-// console.log('Before converting result to uppercase:' + answer);
+// console.log('Original case:' + answer);
 var answer = answer.toUpperCase();
-// console.log('After converting result to uppercase:' + answer);
+// console.log('Converted to uppercase:' + answer);
     if (answer == 'ALASTAIR') {
         alert("Hello Alastair, please give me an A on this assignment!");
     } else if (answer == 'JACOB') {
@@ -13,9 +13,9 @@ var answer = answer.toUpperCase();
     }
 
 var result1 = prompt("Am I from the East Coast?");
-// console.log('Before converting result to uppercase:' + result1);
+// console.log('Original case:' + result1);
 var result1 = result1.toUpperCase();
-// console.log('After converting result to uppercase:' + result1);
+// console.log('Converted to uppercase:' + result1);
 // console.log(result1);
     if (result1 == 'YES') {
         alert("Good job! You're paying attention!");
@@ -30,9 +30,9 @@ var result1 = result1.toUpperCase();
     }
 
 var result2 = prompt("Was I in the military?");
-// console.log('Before converting result to uppercase:' + result2);
+// console.log('Original case:' + result2);
 var result2 = result2.toUpperCase();
-// console.log('After converting result to uppercase:' + result2);
+// console.log('Converted to uppercase:' + result2);
 // console.log(result2);
     if (result2 == 'YES') {
         alert("Good job! You're paying attention!");
@@ -47,9 +47,9 @@ var result2 = result2.toUpperCase();
     }
 
 var result3 = prompt("Did I contract for the Department of Transportation?");
-// console.log('Before converting result to uppercase:' + result3);
+// console.log('Original case:' + result3);
 var result3 = result3.toUpperCase();
-// console.log('After converting result to uppercase:' + result3);
+// console.log('Converted to uppercase:' + result3);
 // console.log(result3);
     if (result3 == 'YES') {
         alert("WROOOOONG! Reload the page to try this question again!");
@@ -64,9 +64,9 @@ var result3 = result3.toUpperCase();
     }
         
 var result4 = prompt("Did I successfully get into coding in the military or government?");
-// console.log('Before converting result to uppercase:' + result4);
+// console.log('Original case:' + result4);
 var result4 = result4.toUpperCase();
-// console.log('After converting result to uppercase:' + result4);
+// console.log('Converted to uppercase:' + result4);
 // console.log(result4);
     if (result4 == 'YES') {
         alert("WROOOOONG! Reload the page to try this question again!");
@@ -81,9 +81,9 @@ var result4 = result4.toUpperCase();
     }
 
 var result5 = prompt("Do I want to become a white hat?");
-// console.log('Before converting result to uppercase:' + result5);
+// console.log('Original case:' + result5);
 var result5 = result5.toUpperCase();
-// console.log('After converting result to uppercase:' + result5);
+// console.log('Converted to uppercase:' + result5);
 // console.log(result5);
     if (result5 == 'YES') {
         alert("Awesome! You know me so well!");
@@ -99,9 +99,9 @@ var result5 = result5.toUpperCase();
 
 
 var goodbye = prompt("Type your first name again!")
-// console.log('Before converting result to uppercase:' + goodbye);
+// console.log('Original case:' + goodbye);
 var goodbye = goodbye.toUpperCase();
-// console.log('After converting result to uppercase:' + goodbye);
+// console.log('Converted to uppercase:' + goodbye);
     if (goodbye == 'ALASTAIR') {
         alert("Bye Alastair, come again soon!");
     } else if (goodbye == 'JACOB') {
