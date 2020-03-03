1d9ed8807b6d753cd5550fd43c00fc0c5669dd7e
racarter1215 <racarter1215@gmail.com>
2020-03-03 13:57:41 -0800

    made files for css and js documents and did first two js prompts for the website

---
+++ js/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var answer = prompt("What is your first name?");
+
+    if (answer == 'Alastair') {
+        alert("Hello Alastair, please give me an A on this assignment!");
+    } else if (answer == "Jacob") {
+        alert("Hello Jacob, please give me an A on this assignment!");
+    } else {
+        alert("If this isn't Jacob or Alastair, you shouldn't be on this site :). You can stay for now, but don't get comfortable!");
+    }
+
+var result1 = prompt("Am I from the East Coast?");
+
+    if (result1 == 'Yes') {
+        alert("Good job! You're paying attention!");
+    } else if (result1 == 'Y') {
+        alert("Good job! You're paying attention!");
+    } else if ('No') {
+        alert("WROOOOONG! Reload the page to try this question again!")
+    } else if ('N') {
+        alert("WROOOOONG! Reload the page to try this question again!");
+    }
+        
