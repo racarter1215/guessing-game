d83ddc59cb5e459c34d3a5664bc7cde435df64f7
racarter1215 <racarter1215@gmail.com>
2020-03-03 14:14:26 -0800

    created two new js codes that trigger correct or incorrect alerts to the user

--- js/app.js
+++ js/app.js
@@ -16,9 +16,52 @@ var result1 = prompt("Am I from the East Coast?");
         alert("Good job! You're paying attention!");
     } else if (result1 == 'Y') {
         alert("Good job! You're paying attention!");
-    } else if ('No') {
-        alert("WROOOOONG! Reload the page to try this question again!")
-    } else if ('N') {
+    } else if (result1 == 'No') {
         alert("WROOOOONG! Reload the page to try this question again!");
+    } else if (result1 == 'N') {
+        alert("WROOOOONG! Reload the page to try this question again!");
+    } else {
+        alert("Huh? Does not compute!!!!");
+    }
+
+var result2 = prompt("Was I in the military?");
+
+    if (result2 == 'Yes') {
+        alert("Good job! You're paying attention!");
+    } else if (result2 == 'Y') {
+        alert("Good job! You're paying attention!");
+    } else if (result2 == 'No') {
+        alert("WROOOOONG! Reload the page to try this question again!");
+    } else if (result2 == 'N') {
+        alert("WROOOOONG! Reload the page to try this question again!");
+    } else {
+        alert("Huh? Does not compute!!!!");
+    }
+
+var result3 = prompt("Did I contract for the Department of Transportation?");
+
+    if (result3 == 'Yes') {
+        alert("WROOOOONG! Reload the page to try this question again!");
+    } else if (result3 == 'Y') {
+        alert("WROOOOONG! Reload the page to try this question again!");
+    } else if (result3 == 'No') {
+        alert("Good job! You're paying attention!");
+    } else if (result3 == 'N') {
+        alert("Good job! You're paying attention!");
+    } else {
+        alert("Huh? Does not compute!!!");
     }
         
+var result4 = prompt("Did I successfully get into coding in the military or government?");
+
+    if (result3 == 'Yes') {
+        alert("WROOOOONG! Reload the page to try this question again!");
+    } else if (result3 == 'Y') {
+        alert("WROOOOONG! Reload the page to try this question again!");
+    } else if (result3 == 'No') {
+        alert("Good job! You're paying attention!");
+    } else if (result3 == 'N') {
+        alert("Good job! You're paying attention!");
+    } else {
+        alert("Huh? Does not compute!!!");
+    }
