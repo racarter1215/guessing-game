b69691c0222b66d1029583da7e442047b3505938
racarter1215 <racarter1215@gmail.com>
2020-03-03 15:11:39 -0800

    finished console logs for all upper case fixes and finished all js prompts and alerts

--- js/app.js
+++ js/app.js
@@ -1,24 +1,26 @@
 'use strict';
 
 var answer = prompt("What is your first name?");
-
-console.log(answer.toLowerCase());
-    if (answer == 'Alastair') {
+// console.log('Before converting result to uppercase:' + answer);
+var answer = answer.toUpperCase();
+// console.log('After converting result to uppercase:' + answer);
+    if (answer == 'ALASTAIR') {
         alert("Hello Alastair, please give me an A on this assignment!");
-    } else if (answer == "Jacob") {
+    } else if (answer == 'JACOB') {
         alert("Hello Jacob, please give me an A on this assignment!");
     } else {
         alert("If this isn't Jacob or Alastair, you shouldn't be on this site :). You can stay for now, but don't get comfortable!");
     }
 
 var result1 = prompt("Am I from the East Coast?");
-
-console.log(result1.toLowerCase());
-    if (result1 == 'Yes') {
+// console.log('Before converting result to uppercase:' + result1);
+var result1 = result1.toUpperCase();
+// console.log('After converting result to uppercase:' + result1);
+    if (result1 == 'YES') {
         alert("Good job! You're paying attention!");
     } else if (result1 == 'Y') {
         alert("Good job! You're paying attention!");
-    } else if (result1 == 'No') {
+    } else if (result1 == 'NO') {
         alert("WROOOOONG! Reload the page to try this question again!");
     } else if (result1 == 'N') {
         alert("WROOOOONG! Reload the page to try this question again!");
@@ -27,13 +29,14 @@ console.log(result1.toLowerCase());
     }
 
 var result2 = prompt("Was I in the military?");
-
-console.log(result2.toLowerCase());
-    if (result2 == 'Yes') {
+// console.log('Before converting result to uppercase:' + result2);
+var result2 = result2.toUpperCase();
+// console.log('After converting result to uppercase:' + result2);
+    if (result2 == 'YES') {
         alert("Good job! You're paying attention!");
     } else if (result2 == 'Y') {
         alert("Good job! You're paying attention!");
-    } else if (result2 == 'No') {
+    } else if (result2 == 'NO') {
         alert("WROOOOONG! Reload the page to try this question again!");
     } else if (result2 == 'N') {
         alert("WROOOOONG! Reload the page to try this question again!");
@@ -42,13 +45,14 @@ console.log(result2.toLowerCase());
     }
 
 var result3 = prompt("Did I contract for the Department of Transportation?");
-
-console.log(result3.toLowerCase());
-    if (result3 == 'Yes') {
+// console.log('Before converting result to uppercase:' + result3);
+var result3 = result3.toUpperCase();
+// console.log('After converting result to uppercase:' + result3);
+    if (result3 == 'YES') {
         alert("WROOOOONG! Reload the page to try this question again!");
     } else if (result3 == 'Y') {
         alert("WROOOOONG! Reload the page to try this question again!");
-    } else if (result3 == 'No') {
+    } else if (result3 == 'NO') {
         alert("Good job! You're paying attention!");
     } else if (result3 == 'N') {
         alert("Good job! You're paying attention!");
@@ -57,13 +61,14 @@ console.log(result3.toLowerCase());
     }
         
 var result4 = prompt("Did I successfully get into coding in the military or government?");
-
-console.log(result4.toLowerCase());
-    if (result4 == 'Yes') {
+// console.log('Before converting result to uppercase:' + result4);
+var result4 = result4.toUpperCase();
+// console.log('After converting result to uppercase:' + result4);
+    if (result4 == 'YES') {
         alert("WROOOOONG! Reload the page to try this question again!");
     } else if (result4 == 'Y') {
         alert("WROOOOONG! Reload the page to try this question again!");
-    } else if (result4 == 'No') {
+    } else if (result4 == 'NO') {
         alert("Good job! You're paying attention!");
     } else if (result4 == 'N') {
         alert("Good job! You're paying attention!");
@@ -72,17 +77,29 @@ console.log(result4.toLowerCase());
     }
 
 var result5 = prompt("Do I want to become a white hat?");
-
-console.log(result5.toLowerCase());
-    if (result5 == 'Yes') {
+// console.log('Before converting result to uppercase:' + result5);
+var result5 = result5.toUpperCase();
+// console.log('After converting result to uppercase:' + result5);
+    if (result5 == 'YES') {
         alert("Awesome! You know me so well!");
     } else if (result5 == 'Y') {
         alert("Awesome! You know me so well!");
-    } else if (result5 == 'No') {
+    } else if (result5 == 'NO') {
         alert("Uh....read my bio and do the test over again. You must have missed something!");
     } else if (result5 == 'N') {
         alert("Uh....read my bio and do the test over again. You must have missed something!");
     } else {
         alert("Huh? Does not compute!!!");
     }
-    
+
+var goodbye = prompt("Type your first name again!")
+// console.log('Before converting result to uppercase:' + goodbye);
+var goodbye = goodbye.toUpperCase();
+// console.log('After converting result to uppercase:' + goodbye);
+    if (goodbye == 'ALASTAIR') {
+        alert("Bye Alastair, come again soon!");
+    } else if (goodbye == 'JACOB') {
+        alert("Bye Jacob, come again soon!");
+    } else {
+        alert("Honestly, you shouldn't have been here in the first place!");
+    }
